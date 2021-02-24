7b6eecbce161930188995c8dd2c0aefa93ba3989
ManosAnast <manosanastasaki@gmail.com>
2021-02-24 21:02:25 +0200

    Test Phase

--- main.c
+++ main.c
@@ -9,43 +9,54 @@ unsigned int Log(unsigned int n)
 
 int main(int argc, char *argv[])
 {
-    if(argc < 5){
-        printf("Input format ./vaccineMonitor -c citizenRecordsFile –b bloomSize \n");
-        return 0;
-    }
+    // if(argc < 5){
+    //     printf("Input format ./vaccineMonitor -c citizenRecordsFile –b bloomSize \n");
+    //     return 0;
+    // }
 
-    FILE * fp; 
+    // FILE * fp; 
     int ch,Size=0,Bloom;
-    char * File;
+    // char * File;
 
-    LinkedList lst;
+    // LinkedList lst;
 
-    /*Convert the arguments that the user gave.*/
-    for (int i = 1; i < argc; i++){
-        if ( !strcmp(argv[i],"-c") ){
-            File=atoi(argv[++i]);
-        }
-        else if ( !strcmp(argv[i],"-b") ){
-            Bloom=atoi(argv[++i]);
-        }
-    }
+    // /*Convert the arguments that the user gave.*/
+    // for (int i = 1; i < argc; i++){
+    //     if ( !strcmp(argv[i],"-c") ){
+    //         File=atoi(argv[++i]);
+    //     }
+    //     else if ( !strcmp(argv[i],"-b") ){
+    //         Bloom=atoi(argv[++i]);
+    //     }
+    // }
 
-    // calculate the number of entrenches that we get from the file in order to take the size that we need for the hash table
-    fp=fopen(File , "r");   
-    while(1) { 
+    // // calculate the number of entrenches that we get from the file in order to take the size that we need for the hash table
+    // fp=fopen(File , "r");   
+    // while(1) { 
 
-        ch = getc(fp);
-        if( feof(fp) ) { 
-            break ;
-        }
-        if(ch == '\n'){  //Finds how many students are added.
-            Size+=1;
-        }
-    }
-    fclose(fp);
+    //     ch = getc(fp);
+    //     if( feof(fp) ) { 
+    //         break ;
+    //     }
+    //     if(ch == '\n'){  //Finds how many students are added.
+    //         Size+=1;
+    //     }
+    // }
+    // fclose(fp);
 
     //Level is log of the number of entries
+    Size=7;
     Level=Log(Size); 
+    SkipList * slist=SLInit(0);
 
+    LinkedList * List=slist->Header;
+    LLInsert(List, 7, 0);
+    LLInsert(List, 1, 0);
+    LLInsert(List, 3, 0);
+    LLInsert(List, 2, 0);
+    LLInsert(List, 5, 0);
+    LLInsert(List, 4, 0);
+    SLInsert(slist);
+    
     return 0;
 }
