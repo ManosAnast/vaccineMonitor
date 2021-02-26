06d818a527455461bcc0920838af951a193c7f89
ManosAnast <manosanastasaki@gmail.com>
2021-02-26 15:59:24 +0200

    Insert Complete

--- Interface.c
+++ Interface.c
@@ -1,7 +1,42 @@
 # include "skiplist.h"
 
+unsigned int Log(unsigned int n) 
+{ 
+    return (n > 1) ? 1 + Log(n / 2) : 0; 
+} 
+
+void Nothing()
+{
+    return;
+}
+
 void Insert(FILE * fp)
 {
+    int ch,Size=0,Bloom;
+    // // calculate the number of entrenches that we get from the file in order to take the size that we need for the hash table
+    // fp=fopen(File , "r");   
+    // while(1) { 
+
+    //     ch = getc(fp);
+    //     if( feof(fp) ) { 
+    //         break ;
+    //     }
+    //     if(ch == '\n'){  //Finds how many students are added.
+    //         Size+=1;
+    //     }
+    // }
+    // fclose(fp);
+
+    //Level is log of the number of entries
+    Size=10;
+    Level=Log(Size); 
+    SkipList * slist=SLInit(0);
+
+    HTCreate(Size);
+
+    LinkedList * List=slist->Header;
+
+
     char *str;
     str=(char *)calloc(150,sizeof(char));
     
@@ -12,10 +47,13 @@ void Insert(FILE * fp)
     }
 
     const char * s=" ";
-    int ch, index=0;
+    int index=0;
     while (1){
         int i=0;
         index=0;
+        if (atoi(Array[0])==2){
+            Nothing();
+        }
         while ((ch = getc(fp)) != EOF)
         {
             if(ch == '\n'){
@@ -26,6 +64,8 @@ void Insert(FILE * fp)
                 i++;
             }
         }
+        str[i]='\0';
+        
         char * token = strtok(str, s);   //we use strtok in order to break the string into the different data in order to give it to the HTInsert
         //now we will use an array of strings that keeps all the variables that we need for the hash table
         while (token != NULL)   //this loop gives the variables that we need to the array from the token(strtok)
@@ -39,12 +79,19 @@ void Insert(FILE * fp)
             break ;
         }
 
+        /* If the citizen has been vaccinated, yes, insert true. Otherwise insert false*/
         if (!strcmp(Array[5],"NO")){
             HTInsert(atoi(Array[index-6]), Array[index-5], Array[index-4], atoi(Array[index-3]), Array[index-2], false /*, Nothing*/); // give the arguments to the htinsert.
         }
         else if (!strcmp(Array[5],"YES")){
             HTInsert(atoi(Array[index-6]), Array[index-5], Array[index-4], atoi(Array[index-3]), Array[index-2], true /*, Date*/); // give the arguments to the htinsert.
         }
+
+        /* After the citizen has been inserted at the hash, insert it to linked list. */
+        LLInsert(List, atoi(Array[index-6]), 0);
         
     }
+    // SLInsert(slist);
+
+    return;
 }
