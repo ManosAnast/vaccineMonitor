6c654cb3cffc557656015cd52929b2b413a86f16
ManosAnast <manosanastasaki@gmail.com>
2021-03-08 20:54:02 +0200

    Insert Date to hash

--- HashImplementation.c
+++ HashImplementation.c
@@ -1,7 +1,37 @@
-# include "skiplist.h"
+# include "Interface.h"
 
 int M;
 Citizens ** Table;
+
+Date * CreateDate(char * DateStr)
+{
+    Date * Timing=(Date *)calloc(1, sizeof(Date));
+    if (!strcmp(DateStr, "Today")){
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        Timing->Days=tm.tm_mday; Timing->Month=tm.tm_mon + 1; Timing->Year=tm.tm_year+1900;
+        return Timing;
+    }
+    
+    const char * s="-";
+    char **Array=BreakString(DateStr, s, 3);
+    if (atoi(Array[0])<=30 && atoi(Array[0])>0 && atoi(Array[1])>0 && atoi(Array[1])<=12){
+        Timing->Days=atoi(Array[0]); Timing->Month=atoi(Array[1]); Timing->Year=atoi(Array[2]);
+        return Timing;
+    }
+    else{
+        printf("Wrong Date\n"); return NULL;
+    }
+}
+
+void PrintDate(Date * Timing)
+{
+    if (Timing != NULL){
+        printf("%d-%d-%d\n", Timing->Days, Timing->Month, Timing->Year);
+        return;
+    }
+    printf("\n"); return;
+}
 
 void HTCreate(int Size)
 {
@@ -9,13 +39,17 @@ void HTCreate(int Size)
     Table=(Citizens **)calloc(M, sizeof(Citizens *));
 }
 
-Citizens * NewRecord(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated/*,Date*/)
+Citizens * NewRecord(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated, char * DateStr)
 {
     Citizens * NewNode=(Citizens *)calloc(1, sizeof(Citizens));
     NewNode->citizenId=Id; NewNode->Age=Age; NewNode->Vaccinated=Vaccinated; NewNode->Next=NULL;
     NewNode->Name=(char *)calloc(strlen(Name), sizeof(char)); strcpy(NewNode->Name, Name);
     NewNode->Country=(char *)calloc(strlen(Country), sizeof(char)); strcpy(NewNode->Country, Country);
     NewNode->Virus=(char *)calloc(strlen(Virus), sizeof(char)); strcpy(NewNode->Virus, Virus);
+    if (!Vaccinated){
+        NewNode->Timing=NULL;  return NewNode;
+    }
+    NewNode->Timing=CreateDate(DateStr);
     return NewNode;
 }
 
@@ -25,11 +59,11 @@ int Hash(int Item)
     return Item % M;
 }
 
-void HTInsert(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated/*,Date*/)
+void HTInsert(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated, char * DateStr)
 {
     int i = Hash(Id);
     if(Table[i] == NULL){   //if the table that we want is null make it using the new function
-        Table[i]=NewRecord(Id, Name, Country, Age, Virus, Vaccinated);
+        Table[i]=NewRecord(Id, Name, Country, Age, Virus, Vaccinated, DateStr);
         return;
     }
     else{
@@ -37,7 +71,7 @@ void HTInsert(int Id, char * Name, char * Country, int Age, char * Virus, bool V
         while (Temp->Next != NULL){ //find the end of the tables chain
             Temp=Temp->Next;
         }
-        Temp->Next=NewRecord(Id, Name, Country, Age, Virus, Vaccinated);
+        Temp->Next=NewRecord(Id, Name, Country, Age, Virus, Vaccinated, DateStr);
         return;
     }
 }
@@ -60,10 +94,10 @@ void HTPrint()
 {
     for (int i = 0; i < M; i++){
         Citizens * Temp = Table[i];
-        printf("- %d %s %s %d %s %d\n",Temp->citizenId,Temp->Name,Temp->Country,Temp->Age,Temp->Virus,Temp->Vaccinated);
+        printf("- %d %s %s %d %s %d ",Temp->citizenId,Temp->Name,Temp->Country,Temp->Age,Temp->Virus,Temp->Vaccinated); PrintDate(Temp->Timing);
         Temp=Temp->Next;
         while (Temp != NULL ){
-            printf("- %d %s %s %d %s %d\n",Temp->citizenId,Temp->Name,Temp->Country,Temp->Age,Temp->Virus,Temp->Vaccinated);
+            printf("- %d %s %s %d %s %d ",Temp->citizenId,Temp->Name,Temp->Country,Temp->Age,Temp->Virus,Temp->Vaccinated); PrintDate(Temp->Timing);
             Temp=Temp->Next;
         }
     }
--- hash.h
+++ hash.h
@@ -6,6 +6,17 @@
 
 extern int Level,BloomNum;
 
+typedef struct date{
+    int Days;
+    int Month;
+    int Year;
+}Date;
+
+Date * CreateDate(char * DateStr);
+
+void PrintDate(Date * Timing);
+
+
 typedef struct hash{
     int citizenId;
     char * Name;
@@ -14,17 +25,17 @@ typedef struct hash{
     char * Virus;
     bool Vaccinated;
     struct hash * Next;
-    // Date;
+    Date * Timing;
 }Citizens;
 
 // Creates the Hash Table
 void HTCreate(int);
 
 // Makes a new node with the given attributes
-Citizens * NewRecord(int, char *, char *, int, char *, bool /*,Date*/);
+Citizens * NewRecord(int, char *, char *, int, char *, bool, char * DateStr);
 
 // Insert at the hash table a new node.
-void HTInsert(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated/*,Date*/);
+void HTInsert(int Id, char * Name, char * Country, int Age, char * Virus, bool Vaccinated, char * DateStr);
 
 // Prints all the nodes of a Table.
 void HTPrint();
