213dd9d89d433abd85b0fc80a77c1aa1571d297b
ManosAnast <manosanastasaki@gmail.com>
2021-03-20 17:35:57 +0200

    Script is done

--- testfile.sh
+++ testfile.sh
@@ -3,7 +3,7 @@
 total=$#
 
 #test number of parametrs
-if [ total < 4 ]
+if [ $total < 4 ]
     then
     echo Bash Format is ./testFile.sh virusesFile countriesFile numLines duplicatesAllowed 
     exit
@@ -51,3 +51,45 @@ if [ $duplicate != 0 ]
     duplicate=1
 fi
 
+virus=($(cat "$virusfile")) # Array with viruses names
+countries=($(cat "$countryfile")) # Array with countries
+viruslength=${#virus[@]}
+countrieslength=${#countries[@]}
+
+# Empty the new file from the previous things that it contained and insert the first entry
+if [ $(shuf -i 0-1 -n 1) == 1 ]
+    then
+    vac="YES"
+    Date="$(shuf -i 1-30 -n 1)-$(shuf -i 1-12 -n 1)-$(shuf -i 2000-2050 -n 1)"
+else
+    vac="NO"
+    Date=""
+fi 
+var="0 name0 surname0 ${countries[$(shuf -i 0-$countrieslength -n 1)]} $(shuf -i 10-80 -n 1) ${virus[$(shuf -i 0-$viruslength -n 1)]} $vac $Date"
+echo $var > $input
+
+# Make of input file
+flag=0
+for ((i=1; i<$lines; i++))
+do
+    if [ $(shuf -i 0-1 -n 1) == 1 ] # Figures out if the citizen is vaccinated or not
+        then
+        vac="YES"
+        Date="$(shuf -i 1-30 -n 1)-$(shuf -i 1-12 -n 1)-$(shuf -i 2000-2050 -n 1)"
+    else
+        vac="NO"
+        Date=""
+    fi 
+
+    if [[ ( $duplicate == 1 ) && ( $(shuf -i 0-1 -n 1) == 1 ) && ( $flag == 0 ) ]] # Checks if duplication is allowed
+        then 
+        flag=1
+        echo $var >> $input
+        continue
+    else
+        flag=0
+        var="$i name$i surname$i ${countries[$(shuf -i 0-$countrieslength -n 1)]} $(shuf -i 10-80 -n 1) ${virus[$(shuf -i 0-$viruslength -n 1)]} $vac $Date"
+        echo $var >> $input
+    fi
+
+done
